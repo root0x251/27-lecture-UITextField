49a12da1cb3440d2d26b7eacf0161b4c186a2a40
Slava <rtootik@gmail.com>
2017-05-29 18:49:30 +0300

    1 part

--- 27.UITextField/ViewController.h
+++ 27.UITextField/ViewController.h
@@ -10,8 +10,6 @@
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
-@property (weak, nonatomic) IBOutlet UITextField *lasNameTextField;
 
 @end
 
--- 27.UITextField/ViewController.m
+++ 27.UITextField/ViewController.m
@@ -8,22 +8,103 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()  <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *nameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
+- (IBAction)loginText:(UIButton *)sender;
+- (IBAction)textChangetAction:(UITextField *)sender;
 
 @end
 
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    self.nameTextField.placeholder = @"Имя";
+    self.nameTextField.textAlignment = NSTextAlignmentLeft;
+    self.nameTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    [self.nameTextField becomeFirstResponder];
+    
+    self.lastNameTextField.placeholder = @"Фамилия";
+    self.lastNameTextField.textAlignment = NSTextAlignmentLeft;
+    self.lastNameTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    
+    
+    
+    NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
+    [notificationCenter addObserver:self
+                           selector:@selector(notificationTextDidBeginEditing:)
+                               name:UITextFieldTextDidBeginEditingNotification
+                             object:nil];
+    
+    [notificationCenter addObserver:self
+                           selector:@selector(notificationTextDidEndEditing:)
+                               name:UITextFieldTextDidEndEditingNotification
+                             object:nil];
+    
+    [notificationCenter addObserver:self
+                           selector:@selector(notificationTextDidChang:)
+                               name:UITextFieldTextDidChangeNotification
+                             object:nil];
 
+}
+//+++++++++++++
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated. 
 }
 
+
+- (IBAction)loginText:(UIButton *)sender {
+    NSLog(@"Имя: %@ \nФамилия: %@", self.nameTextField.text, self.lastNameTextField.text);
+    if ([self.nameTextField isFirstResponder]) {
+        [self.nameTextField resignFirstResponder];
+    } else if ([self.lastNameTextField isFirstResponder]) {
+        [self.lastNameTextField resignFirstResponder];
+    }
+}
+
+- (IBAction)textChangetAction:(UITextField *)sender {
+    NSLog(@"%@", sender.text);
+
+}
+
+#pragma mark - UITextFieldDelegate
+//- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+//    return [textField isEqual:self.nameTextField];
+//}
+//
+//- (BOOL)textFieldShouldClear:(UITextField *)textField {
+//    return NO;
+//}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    // becomeFirstResponder место где стоит курсор
+    if ([textField isEqual:self.nameTextField]) {       // если курсор стоит на self.nameTextField
+        [self.lastNameTextField becomeFirstResponder];  // при нажатии на клавишу next мы переключаемся на поле self.lastNameTextField
+    } else {
+        [textField resignFirstResponder];               // при нажатии на клавишу return закрываем клавиатуру (убираем курсор)
+    }
+    
+    return YES;
+}
+
+#pragma mark - Notification
+
+- (void)notificationTextDidBeginEditing:(NSNotification *)notification {
+    NSLog(@"notificationTextDidBeginEditing");
+}
+
+- (void)notificationTextDidEndEditing:(NSNotification *)notification {
+    NSLog(@"notificationTextDidEndEditing");
+}
+
+- (void)notificationTextDidChang:(NSNotification *)notification {
+    NSLog(@"notificationTextDidChang");
+}
 
 @end
